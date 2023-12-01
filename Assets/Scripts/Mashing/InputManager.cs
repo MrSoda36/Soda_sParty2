3f05829fc934a1f25a8d292e2609c01c7310a179
JaquyLaTortue <93046036+JaquyLaTortue@users.noreply.github.com>
2023-12-01 12:50:38 +0100

    Banger importation + win display script

--- Assets/Scripts/Mashing/InputManager.cs
+++ Assets/Scripts/Mashing/InputManager.cs
@@ -1,8 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    [SerializeField]GameObject[] players = new GameObject[4];
+    //Store the players references
+    [SerializeField] GameObject[] players = new GameObject[4];
+
+    //Store the ShakeByInputSystem references
+    [SerializeField] ShakeByInputSystem ShakeScript;
+
+
+    public Gamepad[] gamepadCount;
+    private void Start()
+    {
+        gamepadCount = InputSystem.devices.OfType<Gamepad>().ToArray();
+        foreach (var item in gamepadCount)
+        {
+            Debug.Log(item.name);
+
+        }
+    }
+    public void FirstPlayerInput()
+    {
+        ShakeScript.OnMashingFirstPlayer();
+    }
+    public void SecondPlayerInput()
+    {
+        ShakeScript.OnMashingSecondPlayer();
+    }
+    public void ThirdPlayerInput()
+    {
+        if (players[2].activeSelf)
+        {
+            ShakeScript.OnMashingThirdPlayer();
+        }
+    }
+    public void FourthPlayerInput()
+    {
+        if (players[3].activeSelf)
+        {
+            ShakeScript.OnMashingFourthPlayer();
+        }
+    }
 }
