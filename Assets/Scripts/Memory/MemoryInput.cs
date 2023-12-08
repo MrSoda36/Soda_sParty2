dee8aad94ffb4688d9086dce2e611b39ab934148
Yoann | Finiarael <yoann.lemetayer@e-artsup.net>
2023-12-08 10:07:36 +0100

    Jeu fonctionnel (1 bug vu qui reste) et relativement propre

--- Assets/Scripts/Memory/MemoryInput.cs
+++ Assets/Scripts/Memory/MemoryInput.cs
@@ -9,12 +9,6 @@ public class MemoryInput : MonoBehaviour
 
     public event Action OnKeyPressed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/Scripts/Memory/MemoryLauncher.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryLauncher : MonoBehaviour
+{
+    [SerializeField] private MemoryBehaviour _behaviour;
+    [SerializeField] private MemoryUI _ui;
+    [SerializeField] private MemoryInput _input;
+
+    private bool _waitingForKey = false;
+    private bool _keyPressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _input.OnKeyPressed += KeyListener;
+        StartCoroutine(InitGame());
+    }
+
+    private IEnumerator InitGame()
+    {
+        _behaviour.ExtractCardInfo();
+
+        _ui.Initialize();
+        yield return StartCoroutine(WaitForInput());
+        _ui.HidePanel();
+
+        StartCoroutine(_behaviour.GamePlay());
+
+        yield return null;
+    }
+
+    private IEnumerator WaitForInput()
+    {
+        _waitingForKey = true;
+
+        yield return new WaitUntil(() => _keyPressed);
+
+        _keyPressed = false;
+        _waitingForKey = false;
+    }
+
+    private void KeyListener()
+    {
+        if (_waitingForKey)
+        {
+            _keyPressed = true;
+        }
+    }
+}
